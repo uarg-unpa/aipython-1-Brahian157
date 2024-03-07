26a6d79f27c466038ebcfc666c70a86b052c2a1e
Brahian157 <161514176+Brahian157@users.noreply.github.com>
2024-03-07 19:51:48 +0000

    trabajo en clase

---
+++ entrega/Guia I/Guia I.py
@@ -0,0 +1,9 @@
+print("Las máquinas me sorprenden con mucha frecuencia.")
+print("")
+print("Soy Ezequiel")
+print("23",23)
+print("Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano.")
+print("Brahian","Ezequiel",18,sep="_")
+print("libertad","\n1760","\n9400",sep="\t")
+print("Feliz","Primavera",2024,sep="\n")
+print
